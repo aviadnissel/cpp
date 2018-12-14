5592c0c87c5ccfdd96b634e63009a4e9aabbed25
avian <aviadnissel@gmail.com>
2018-12-14 17:24:38 +0200

    added Matrix3D with full header

---
+++ ex1/Matrix3D.h
@@ -0,0 +1,55 @@
+//
+// Created by Avian on 14/12/2018.
+//
+
+#ifndef CPP_MATRIX3D_H
+#define CPP_MATRIX3D_H
+
+#include "Vector3D.h"
+
+class Matrix3D {
+private:
+    Vector3D vectors[3];
+
+public:
+    Matrix3D();
+    Matrix3D(double num);
+    Matrix3D(double num1, double num2, double num3,
+             double num4, double num5, double num6,
+             double num7, double num8, double num9);
+    Matrix3D(double numbers[9]);
+    Matrix3D(double numbers[3][3]);
+    Matrix3D(Vector3D vectors[3]);
+    Matrix3D(const Matrix3D &other);
+
+    Matrix3D& operator+=(const Matrix3D &other);
+    Matrix3D& operator-=(const Matrix3D &other);
+
+    Matrix3D operator*=(const Matrix3D &other) const;
+
+    Matrix3D operator+(const Matrix3D &other) const;
+    Matrix3D operator-(const Matrix3D &other) const;
+    Matrix3D operator*(const Matrix3D &other) const;
+
+    Matrix3D& operator*=(double num);
+    Matrix3D& operator/=(double num);
+
+    Vector3D operator*(const Vector3D &other) const;
+
+    Matrix3D& operator=(const Matrix3D &other);
+
+
+    Vector3D operator[](int i) const;
+    Vector3D& operator[](int i);
+
+    Vector3D row(short row);
+    Vector3D column(short row);
+    double trace();
+    double determinant();
+};
+
+
+std::istream& operator>>(std::istream &is, Matrix3D &vec);
+std::ostream& operator<<(std::ostream &os, Matrix3D &vec);
+
+#endif
