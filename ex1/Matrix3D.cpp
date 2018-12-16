7b81306d7e1788db4487e65c7444ae6ef5bdb44f
avian <aviadnissel@gmail.com>
2018-12-16 21:36:00 +0200

    more matrix implementation

--- ex1/Matrix3D.cpp
+++ ex1/Matrix3D.cpp
@@ -23,12 +23,24 @@ Matrix3D::Matrix3D(double num)
 }
 Matrix3D::Matrix3D(double num1, double num2, double num3,
          double num4, double num5, double num6,
-         double num7, double num8, double num9) : Matrix3D({num1, num2, num3,
-                                                            num4, num5, num6,
-                                                            num7, num8, num9})
-{}
+         double num7, double num8, double num9)
+{
+    // TODO Make it better
+    vectors[0][0] = num1;
+    vectors[0][1] = num2;
+    vectors[0][2] = num3;
 
-Matrix3D::Matrix3D(double numbers[9])
+    vectors[1][0] = num4;
+    vectors[1][1] = num5;
+    vectors[1][2] = num6;
+
+    vectors[2][0] = num7;
+    vectors[2][1] = num8;
+    vectors[2][2] = num9;
+
+}
+
+Matrix3D::Matrix3D(const double numbers[9])
 {
     for(int i = 0; i < 3; i++)
     {
@@ -39,7 +51,7 @@ Matrix3D::Matrix3D(double numbers[9])
     }
 }
 
-Matrix3D::Matrix3D(double numbers[3][3])
+Matrix3D::Matrix3D(const double numbers[3][3])
 {
     for(int i = 0; i < 3; i++)
     {
@@ -50,19 +62,104 @@ Matrix3D::Matrix3D(double numbers[3][3])
     }
 }
 
-Matrix3D::Matrix3D(Vector3D vectors[3])
+Matrix3D::Matrix3D(const Vector3D vectors[3])
 {
+    // TODO can that be merged with [3][3]?
     for(int i = 0; i < 3; i++)
     {
         this->vectors[i] = vectors[i];
     }
 }
 
-Matrix3D::Matrix3D(const Matrix3D &other)
+Matrix3D::Matrix3D(const Matrix3D &other) : Matrix3D(other.vectors)
+{
+}
+
+Matrix3D& Matrix3D::operator+=(const Matrix3D &other)
 {
     for(int i = 0; i < 3; i++)
     {
-        vectors[i] = other.vectors[i];
+        for(int j = 0; j < 3; j++)
+        {
+            vectors[i][j] += other[i][j];
+        }
     }
 }
+Matrix3D& Matrix3D::operator-=(const Matrix3D &other)
+{
+    return *this += -other;
+}
 
+Matrix3D Matrix3D::operator-() const
+{
+    Matrix3D newMatrix;
+    for(int i = 0; i < 3; i++)
+    {
+        for(int j = 0; j < 3; j++)
+        {
+            newMatrix[i][j] = -vectors[i][j];
+        }
+    }
+    return newMatrix;
+}
+
+Matrix3D& Matrix3D::operator*=(const Matrix3D &other)
+{
+    *this = *this * other;
+    return *this;
+}
+
+Matrix3D Matrix3D::operator+(const Matrix3D &other) const
+{
+    Matrix3D newMatrix(*this);
+    for(int i = 0; i < 3; i++)
+    {
+        newMatrix[i] += other[i];
+    }
+    return newMatrix;
+}
+Matrix3D Matrix3D::operator-(const Matrix3D &other) const
+{
+    return *this + (-other);
+}
+Matrix3D Matrix3D::operator*(const Matrix3D &other) const
+{
+    // TODO use * Vector3D
+    Matrix3D newMatrix;
+    for(int i = 0; i < 3; i++)
+    {
+        for(int j = 0; j < 3; j++)
+        {
+            newMatrix[i][j] = row(i) * other.column(j);
+        }
+    }
+    return newMatrix;
+}
+
+Matrix3D& Matrix3D::operator*=(double num)
+{
+    for(int i = 0; i < 3; i++)
+    {
+        vectors[i] *= num;
+    }
+    return *this;
+}
+
+Matrix3D& Matrix3D::operator/=(double num)
+{
+    if(num == 0)
+    {
+        return *this;
+    }
+    return *this *= (1/num);
+}
+
+Vector3D Matrix3D::operator*(const Vector3D &other) const
+{
+    Vector3D newVector;
+    for(int i = 0; i < 3; i++)
+    {
+        newVector[i] = vectors[i] * other;
+    }
+    return newVector;
+}
--- ex1/Matrix3D.h
+++ ex1/Matrix3D.h
@@ -17,15 +17,17 @@ public:
     Matrix3D(double num1, double num2, double num3,
              double num4, double num5, double num6,
              double num7, double num8, double num9);
-    Matrix3D(double numbers[9]);
-    Matrix3D(double numbers[3][3]);
-    Matrix3D(Vector3D vectors[3]);
+    Matrix3D(const double numbers[9]);
+    Matrix3D(const double numbers[3][3]);
+    Matrix3D(const Vector3D vectors[3]);
     Matrix3D(const Matrix3D &other);
 
     Matrix3D& operator+=(const Matrix3D &other);
     Matrix3D& operator-=(const Matrix3D &other);
 
-    Matrix3D operator*=(const Matrix3D &other) const;
+    Matrix3D operator-() const;
+
+    Matrix3D& operator*=(const Matrix3D &other);
 
     Matrix3D operator+(const Matrix3D &other) const;
     Matrix3D operator-(const Matrix3D &other) const;
@@ -42,10 +44,10 @@ public:
     Vector3D operator[](int i) const;
     Vector3D& operator[](int i);
 
-    Vector3D row(short row);
-    Vector3D column(short row);
-    double trace();
-    double determinant();
+    Vector3D row(short row) const;
+    Vector3D column(short row) const;
+    double trace() const;
+    double determinant() const;
 };
 
 
