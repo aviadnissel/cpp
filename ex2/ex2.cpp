d754efc80166da53539a43c64547d4f95a516661
Aviad Nissel <aviadnissel@cs.huji.ac.il>
2018-12-31 21:27:33 +0200

    works, with wrong result, and a lot of time

--- ex2/ex2.cpp
+++ ex2/ex2.cpp
@@ -1,26 +1,36 @@
 #include <iostream>
+#include <numeric>
 #include <map>
+#include <math.h>
 #include <vector>
 #include <fstream>
+#include <utility>
 #include <boost/tokenizer.hpp>
 #include <boost/algorithm/string.hpp>
 
 using std::map;
 using std::vector;
 using std::string;
+using std::cout;
+using std::endl;
+using std::pair;
 
+int getCountOrZero(map<string, int> countMap, string toSearch)
+{
+	auto it = countMap.find(toSearch);
+	if (it != countMap.end())
+	{
+		return it->second;
+	}
+	return 0;
+}
 map<string, int> countWords(vector<string> words)
 {
 	map<string, int> wordCount;
 	int count;
 	for(string word: words)
 	{
-		auto it = wordCount.find(word);
-		count = 0;
-		if (it != wordCount.end())
-		{
-			count = it->second;
-		}
+		count = getCountOrZero(wordCount, word);
 		wordCount[word] = count + 1;
 	}
 	return wordCount;
@@ -51,6 +61,34 @@ vector<string> readWords(const string &filename)
 	return words;
 }
 
+vector<int> createVector(const map<string, int> textCount, const vector<string> commonWords) // TODO rename
+{
+	vector<int> wordVector;
+	for(string word: commonWords)
+	{
+		wordVector.push_back(getCountOrZero(textCount, word));
+	}
+	return wordVector;
+		
+}
+
+
+double calculateAngle(vector<int> wordVector1, vector<int> wordVector2)
+{
+	double product = inner_product(wordVector1.begin(), wordVector1.end(), wordVector2.begin(), 0);
+	double norm = inner_product(wordVector1.begin(), wordVector1.end(), wordVector1.begin(), 0) * inner_product(wordVector2.begin(), wordVector2.end(), wordVector2.begin(), 0);
+	return product / norm;
+}
+
+vector<int> getTextVector(string fileName, vector<string> commonWords)
+{
+	cout << "reading words" << endl;
+        vector<string> textWords = readWords(fileName);
+	cout << "counting words" << endl;
+        map<string, int> textCount = countWords(textWords);
+	cout << "creating vector" << endl;
+        return createVector(textCount, commonWords);
+}
 int main(int argc, char* argv[])
 {
 	if(argc < 4) // TODO const
@@ -61,18 +99,20 @@ int main(int argc, char* argv[])
 	}
 
 	vector<string> commonWords = readWords(argv[1]); // TODO const
-	vector<string> unknownTextWords = readWords(argv[2]);
-	map<string, int> unkownTextCount = countWords(unknownTextWords);
-/*	for(auto const &word: unknownTextWords)
-	{
-		std::cout << word << " ";
-	}
-	std::cout << std::endl;
-*/
-	for(auto const &pair: unkownTextCount)
-	{
-		std::cout << pair.first << " " << pair.second << std::endl;
-	}
+	vector<int> unknownTextVector = getTextVector(argv[2], commonWords);
 
+	pair<string, double> highestAngle = std::make_pair("start", -1);
+	for(int i = 3; i < argc; i++)
+	{
+		string fileName = argv[i];
+		vector<int> textVector = getTextVector(fileName, commonWords);
+		double textAngle = calculateAngle(textVector, unknownTextVector);
+		cout << fileName << " " << textAngle << endl;
+		if (textAngle >= highestAngle.second)
+		{
+			highestAngle = make_pair(fileName, textAngle);
+		}
+	}
+	cout << "Best matching author is " << highestAngle.first << " score " << highestAngle.second << endl;
 	return 0;
 }
