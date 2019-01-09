3533b0292ab0c2e539a85a4c4bd212accade4c7f
Aviad Nissel <aviadnissel@cs.huji.ac.il>
2019-01-09 16:22:33 +0200

    moved all implementation to hpp, created main file for tests

--- ex3/Matrix.hpp
+++ ex3/Matrix.hpp
@@ -2,19 +2,60 @@
 #define CPP_MATRIX_H
 
 #include <vector>
+#include <iostream>
 
 using std::vector;
 
 template <class T>
 class Matrix {
 public:
-    Matrix();
-    Matrix(unsigned int rows, unsigned int cols);
-    Matrix(Matrix& other);
+	Matrix<T>();
+	Matrix<T>(unsigned int rows, unsigned int cols);
+	Matrix<T>(Matrix<T>& other);
+
+	template <class S>
+	friend std::ostream& operator<<(std::ostream &os, const Matrix<S> &mat);
+
+
 private:
-    vector<vector<T>> rows;
+	vector<vector<T>> rows;
 
 };
 
+template <class T>
+Matrix<T>::Matrix()
+{
+	vector<T> row;
+	row.push_back(T(0));
+	rows.push_back(row);
+}
+
+template <class T>
+Matrix<T>::Matrix(unsigned int rows, unsigned int cols)
+{
+	for(unsigned int i = 0; i < rows; i++)
+	{
+		vector<T> row;
+		for(unsigned int j = 0; j < cols; j++)
+		{
+			row.push_back(T(0));
+		}
+		this->rows.push_back(row);
+	}
+}
+
+template <class T>
+std::ostream& operator<<(std::ostream &os, const Matrix<T> &mat)
+{
+	for(vector<T> row: mat.rows)
+	{
+		for(T cell: row)
+		{
+			std::cout << cell << "\t";
+		}
+		std::cout << std::endl;
+	}
+	return os;
+}
 
 #endif //CPP_MATRIX_H
---
+++ ex3/main.cpp
@@ -0,0 +1,9 @@
+#include "Matrix.hpp"
+#include <iostream>
+
+int main()
+{
+	Matrix<int> m(3, 4);
+	std::cout << m;
+	return 0;
+}
